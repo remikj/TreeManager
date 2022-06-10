eb441a3c1477824ab7f5ba7828d59634ce76a4ba
Remigiusz Janeczek <remigiusz.janeczek@gmail.com>
2022-06-10 09:38:31 +0200

    Initialize TreeManager Backend project

---
+++ TreeManager-BE/src/main/java/rj/treemanager/TreeManagerBeApplication.java
@@ -0,0 +1,13 @@
+package rj.treemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeManagerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeManagerBeApplication.class, args);
+    }
+
+}
---
+++ TreeManager-BE/src/test/java/rj/treemanager/TreeManagerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package rj.treemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeManagerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
